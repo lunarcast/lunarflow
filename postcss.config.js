dec0ffd425234e0e4f4d631c380279b4fed4019f
Matei Adriel <rafaeladriel11@gmail.com>
2020-10-11 13:37:59 +0300

    build: postcss setup

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+const production = process.env.NODE_ENV === "production";
+
+const prodPlugins = production
+  ? [
+      require("cssnano")({
+        preset: "default",
+      }),
+    ]
+  : [];
+
+module.exports = {
+  plugins: [
+    require("postcss-import")({}),
+    require("postcss-simple-vars"),
+    require("postcss-font-magician")({
+      foundries: "bootstrap google",
+    }),
+    require("postcss-preset-env")({}),
+    ...prodPlugins,
+  ],
+};
