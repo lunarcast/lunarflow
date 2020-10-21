b4491b4bed03025888cf526f1b69041ca1801d4e
Matei Adriel <rafaeladriel11@gmail.com>
2020-10-21 11:44:23 +0300

    feat(utils): compile time warnings when I use the Label typeclass

--- packages/utils/src/Label.purs
+++ packages/utils/src/Label.purs
@@ -1,9 +1,17 @@
 module Lunarflow.Label where
 
+import Prim.TypeError (class Warn, Above, Beside, QuoteLabel, Text)
+
 -- TODO: Think more about how good / bad of an idea this is.
 -- WARNING: Don't use this for actual docs, just for conveying 
 -- a small amoung of info to the programmer which actually calls the function.
 -- | Dummy typeclass used for giving extra info to the programmer.
 class Label (a :: Symbol)
 
-instance label :: Label a
+instance label ::
+  Warn
+      ( Above
+          (Text "TODO: create a newtype instead of using the label ")
+          (Beside (Text "    \"") (Beside (QuoteLabel a) (Text "\"")))
+      ) =>
+  Label a
