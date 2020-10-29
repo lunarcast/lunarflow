79a4405f237af3b09380b2d7646cf4178a30e83b
Matei Adriel <rafaeladriel11@gmail.com>
2020-10-29 16:15:34 +0200

    build: Setup single line css comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,22 @@
-const production = process.env.NODE_ENV === "production";
+const production = process.env.NODE_ENV === "production"
 
 const prodPlugins = production
   ? [
       require("cssnano")({
-        preset: "default",
-      }),
+        preset: "default"
+      })
     ]
-  : [];
+  : []
 
 module.exports = {
+  parser: require("postcss-comment"),
   plugins: [
     require("postcss-import")({}),
     require("postcss-simple-vars"),
     require("postcss-font-magician")({
-      foundries: "bootstrap google",
+      foundries: "bootstrap google"
     }),
     require("postcss-preset-env")({}),
-    ...prodPlugins,
-  ],
-};
+    ...prodPlugins
+  ]
+}
