89e4485121e62723c405ee544870f837a63dc2c7
Matei Adriel <rafaeladriel11@gmail.com>
2020-11-02 23:11:39 +0200

    feat(utils): Endomorphism type and helper for printing a value with a Debug instance

--- packages/utils/src/Debug.purs
+++ packages/utils/src/Debug.purs
@@ -8,10 +8,14 @@ import Effect (Effect)
 import Effect.Console as Console
 import Effect.Unsafe (unsafePerformEffect)
 
+-- | Like show but for stuff with debug instances.
+showPretty :: forall d. Debug d => d -> String
+showPretty = prettyPrintWith { compactThreshold: 6, maxDepth: Just 1000 } <<< debug
+
 -- | A debug instance which shows more context.
 -- | At repl, call `:print Lunarflow.Debug.myDebug`
 myDebug :: forall d. Debug d => d -> Effect Unit
-myDebug = Console.log <<< prettyPrintWith { compactThreshold: 6, maxDepth: Just 1000 } <<< debug
+myDebug = Console.log <<< showPretty
 
 -- | Similar to spy but requires a debug instance.
 debugSpy :: forall a. Debug a => DebugWarning => a -> a
--- packages/utils/src/Function.purs
+++ packages/utils/src/Function.purs
@@ -1,7 +1,11 @@
 -- | Don't kill me please
-module Lunarflow.Pipe where
+module Lunarflow.Function where
 
 import Data.Function (applyFlipped)
 
 -- infixr 0 apply as <|
 infixl 1 applyFlipped as |>
+
+-- | fp-ts had this and it's pretty handy
+type Endomorphism a
+  = a -> a
