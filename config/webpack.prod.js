ad1665cfc9ab2110395f7522222ee01f88ff3e59
Matei Adriel <rafaeladriel11@gmail.com>
2020-11-08 22:48:45 +0200

    build: I just hope this deploys...

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: "/",
+    publicPath: "/lunarflow",
     filename: "js/[name].[contenthash].bundle.js"
   },
   plugins: [
