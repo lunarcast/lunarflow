3afb4d3ffcc2f3ecf14fad313ff1a97d4e67113a
Matei Adriel <rafaeladriel11@gmail.com>
2020-11-11 10:32:13 +0200

    build: yk what this is

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: "/",
+    publicPath: "",
     filename: "js/[name].[contenthash].bundle.js"
   },
   plugins: [
